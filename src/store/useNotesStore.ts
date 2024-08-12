a62901aaf2d8a9ffac6a8d330812ff4885985872
Rochak Pandey <rochakpandey56@gmail.com>
2024-08-13 02:01:56 +0530

    ✨: added zustand for store setup

---
+++ src/store/useNotesStore.ts
@@ -0,0 +1,44 @@
+import { trace } from "console";
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+
+export const initZustandDevTools = (stores = []) => {
+  // @ts-ignore
+  return true;
+};
+interface NotesStoreState {
+  currentUserId: string;
+  activeNoteContent: string;
+  activeNoteId: string;
+  fileDirectory: any[];
+}
+interface Folder {
+  id: string;
+  title: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+interface Note {
+  id: string;
+  title: string;
+  userId: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  folderId: string;
+}
+
+export const useNotesStore = create(
+  devtools(
+    immer((set) => ({
+      // write your store logic here
+      currentUserId: "",
+      activeNoteId: "",
+      fileDirectory: [],
+      // store ends
+    })),
+    { name: "notesStore", enabled: initZustandDevTools(), trace: true }
+  )
+);
